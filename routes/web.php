5af86d15a32e3ae5c6df1cb8fc82e2258ece9c96
martin <martin_andanio@tokoonderdil.id>
2023-02-20 16:06:43 +0700

    config

--- routes/web.php
+++ routes/web.php
@@ -94,3 +94,25 @@ Route::get('/permission', function () {
 Route::get('/admin/seed', function () {
     Artisan::call('db:seed --class=CreateAdminUserSeeder');
 });
+Route::get('/clear-cache', function() {
+    Artisan::call('cache:clear');
+    return 'Application cache has been cleared';
+});
+
+//Clear route cache:
+Route::get('/route-cache', function() {
+    Artisan::call('route:cache');
+    return 'Routes cache has been cleared';
+});
+
+//Clear config cache:
+Route::get('/config-cache', function() {
+    Artisan::call('config:cache');
+    return 'Config cache has been cleared';
+});
+
+// Clear view cache:
+Route::get('/view-clear', function() {
+    Artisan::call('view:clear');
+    return 'View cache has been cleared';
+});
