2048453bab774cd9a91ad907502569912148bbb7
martin <martin_andanio@tokoonderdil.id>
2023-03-03 13:04:54 +0700

    new version

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -22,6 +22,7 @@ class Kernel extends ConsoleKernel
 
     protected function schedule(Schedule $schedule)
     {
+        Log::info("schedule run");
         $schedule->command('attendance-job')
             ->dailyAt('01:00');
         $schedule->command('create-permission-routes')
